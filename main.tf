6bb0b0faa5e9629f65c85ecdb3a3f7074cad7fd2
Karan Gupta <karangupta@Karans-MacBook-Pro.local>
2026-09-01 21:10:44 +0530

    update main.tf add a new compute pool, max_cfu = 5

--- main.tf
+++ main.tf
@@ -76,7 +76,7 @@ resource "confluent_flink_compute_pool" "flink_coveo_dev" {
   display_name = "my_compute_pool"
   cloud        = local.cloud
   region       = local.region
-  max_cfu      = 1
+  max_cfu      = 5
 
   environment {
     id = data.confluent_environment.atlas-dev-v2.id
